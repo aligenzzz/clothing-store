c217c8076ecdd8403ceab4216cbf2d128102155b
aligenzzz <alina_bybko@mail.ru>
2023-04-04 21:44:03 +0300

    correct CustomerPageController.java

--- src/main/java/com/example/test/controllers/CustomerPageController.java
+++ src/main/java/com/example/test/controllers/CustomerPageController.java
@@ -8,10 +8,12 @@ import com.example.test.entities.Item;
 import com.example.test.enums.AccessType;
 import com.example.test.interfaces.IListener;
 import com.example.test.interfaces.User;
+import javafx.animation.AnimationTimer;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -21,12 +23,15 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class CustomerPageController implements Initializable
@@ -47,12 +52,12 @@ public class CustomerPageController implements Initializable
     private Button settingsButton;
     @FXML
     private Button returnButton;
-
     @FXML
     private ScrollPane scrollPane;
     @FXML
     private GridPane gridPane;
     private IListener listener;
+    Customer customer = (Customer) GlobalEntities.USER;
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
@@ -64,7 +69,7 @@ public class CustomerPageController implements Initializable
             @Override
             protected Void call() throws Exception {
                 DatabaseConnector databaseConnector = new DatabaseConnector();
-                Customer customer = (Customer) GlobalEntities.USER;
+
                 customer.setFavouriteItems(databaseConnector.getFavouriteItems(customer.id));
                 customer.setShoppingItems(databaseConnector.getShoppingItems(customer.id));
                 customer.setPurchasedItems(databaseConnector.getPurchasedItems(customer.id));
@@ -108,6 +113,14 @@ public class CustomerPageController implements Initializable
         scrollPane.setDisable(true);
         anchorPane.setVisible(true);
     }
+    @FXML
+    Button closeItemCardButton;
+    public void closeItemCardButtonOnAction()
+    {
+        scrollPane.setDisable(false);
+        anchorPane.setVisible(false);
+    }
+
     public void profileButtonOnAction()
     {
         if (profileButton.getTextFill() == Constants.ACTIVECOLOR) return;
@@ -118,12 +131,24 @@ public class CustomerPageController implements Initializable
     {
         if (favouriteItemsButton.getTextFill() == Constants.ACTIVECOLOR) return;
         disactiveButtons();
+
+        animation.stop();
+        gridPane.getChildren().clear();
+        animation = new GridAnimation(customer.getFavouriteItems());
+        animation.start();
+        favouriteItemsButton.setTextFill(Constants.ACTIVECOLOR);
     }
 
     public void shoppingItemsButtonOnAction()
     {
         if (shoppingItemsButton.getTextFill() == Constants.ACTIVECOLOR) return;
         disactiveButtons();
+
+        animation.stop();
+        gridPane.getChildren().clear();
+        animation = new GridAnimation(customer.getShoppingItems());
+        animation.start();
+        shoppingItemsButton.setTextFill(Constants.ACTIVECOLOR);
     }
 
     public void ordersButtonOnAction()
@@ -136,6 +161,12 @@ public class CustomerPageController implements Initializable
     {
         if (purchasedItemsButton.getTextFill() == Constants.ACTIVECOLOR) return;
         disactiveButtons();
+
+        animation.stop();
+        gridPane.getChildren().clear();
+        animation = new GridAnimation(customer.getPurchasedItems());
+        animation.start();
+        purchasedItemsButton.setTextFill(Constants.ACTIVECOLOR);
     }
 
     public void shopsButtonOnAction()
@@ -182,5 +213,63 @@ public class CustomerPageController implements Initializable
         shopsButton.setTextFill(Color.WHITE);
         settingsButton.setTextFill(Color.WHITE);
         returnButton.setTextFill(Color.WHITE);
+    }
+
+    GridAnimation animation = new GridAnimation(new ArrayList<>());
+
+    private class GridAnimation extends AnimationTimer
+    {
+        private final List<Item> itemList;
+        private int size = 0;
+        private int i = 0;
+        int column = 0;
+        int row = 0;
+        public GridAnimation(List<Item> itemList)
+        {
+            this.itemList = itemList;
+            if (itemList != null) this.size = itemList.size();
+        }
+        @Override
+        public void handle(long now)
+        {
+            scrollPane.setHvalue(0);
+            scrollPane.setVvalue(0);
+
+            if (this.itemList == null) return;
+
+            try { doHandle(); }
+            catch (IOException e) { throw new RuntimeException(e); }
+
+            gridPane.setMinWidth(Region.USE_COMPUTED_SIZE);
+            gridPane.setPrefWidth(Region.USE_COMPUTED_SIZE);
+            gridPane.setMaxWidth(Region.USE_PREF_SIZE);
+
+            gridPane.setMinHeight(Region.USE_COMPUTED_SIZE);
+            gridPane.setPrefHeight(Region.USE_COMPUTED_SIZE);
+            gridPane.setMaxHeight(Region.USE_PREF_SIZE);
+        }
+
+        private void doHandle() throws IOException
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(new File(Constants.ITEM).toURI().toURL());
+            AnchorPane anchorPane = fxmlLoader.load();
+
+            Item item = itemList.get(i);
+            if (column == 4)
+            {
+                column = 0;
+                row++;
+            }
+
+            ItemController itemController = fxmlLoader.getController();
+            itemController.setData(item, listener);
+            gridPane.add(anchorPane, column++, row);
+
+            GridPane.setMargin(anchorPane, new Insets(10));
+
+            i++;
+            if (i >= size) stop();
+        }
     }
 }
