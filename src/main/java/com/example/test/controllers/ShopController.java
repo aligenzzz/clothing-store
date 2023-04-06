905312728f543807e1ca79fcc5f8522fe58b543d
aligenzzz <alina_bybko@mail.ru>
2023-04-06 10:40:32 +0300

    add shop.fxml and ShopController.java to show favourites shops of Customer

--- src/main/java/com/example/test/controllers/ShopController.java
+++ src/main/java/com/example/test/controllers/ShopController.java
@@ -1,6 +1,27 @@
 package com.example.test.controllers;
 
+import com.example.test.Constants;
+import com.example.test.entities.Shop;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class ShopController
 {
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Label shopNameLabel;
+    public void setData(Shop shop)
+    {
+        Image image = new Image(Constants.SHOPSIMAGEPATH + shop.getImageSource());
+        imageView.setImage(image);
+        imageView.setStyle("-fx-background-position: CENTER;");
+        shopNameLabel.setText(shop.getName());
+    }
+    public void toShopButtonOnAction()
+    {
 
+    }
 }
